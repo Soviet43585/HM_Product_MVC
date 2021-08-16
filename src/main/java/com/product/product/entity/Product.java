0e435eb900312b7c1926ec1491bd124f81ccfa75
EgorKozlov <egor43585@bk.ru>
2021-08-16 21:59:47 +0300

    Initial commit

---
+++ src/main/java/com/product/product/entity/Product.java
@@ -0,0 +1,23 @@
+package com.product.product.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String title;
+    String description;
+}
